3eac312df89e08f7fc23cf4978de6257b6b21451
Godspeace Adetayo <adetayogodspeace2003@gmail.com>
2025-01-17 02:14:15 +0100

    Deleted the darkmode Imp

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
-import { Button } from "@chakra-ui/react";
-import { useColorMode } from "@/components/ui/color-mode";
 
 const NavBar = () => {
-  const { toggleColorMode } = useColorMode();
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
       <Text> Navbar</Text>
-      <Button variant="outline" onClick={toggleColorMode}>
-        Toggle Mode
-      </Button>
     </HStack>
   );
 };
